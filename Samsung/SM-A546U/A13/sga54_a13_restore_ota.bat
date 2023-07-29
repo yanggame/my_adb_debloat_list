a71f8c96a4b3a4b54c61f937b56076db4cd2b4e3
yanggame <yanggame@gmail.com>
2023-07-29 09:41:27 -0400

    add commands for restoring ota function only

---
+++ Samsung/SM-A546U/A13/sga54_a13_restore_ota.bat
@@ -0,0 +1,3 @@
+adb shell cmd package install-existing adb shell pm uninstall --user 0 com.samsung.oda.service
+adb shell cmd package install-existing adb shell pm uninstall --user 0 com.sec.android.soagent
+adb shell cmd package install-existing adb shell pm uninstall --user 0 com.wssyncmldm
